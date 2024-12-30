82a9ba79c865e2d3e7f772e07255ff4a8d9a4712
Joydip Mondal <joydipmondal27092002@gmail.com>
2024-12-30 14:53:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -9,4 +9,6 @@ export const fetchStudents = () => API.get('/students');
 export const addStudent = (studentData) => API.post('/students', studentData);
 export const markAttendance = (id,attendanceData) => API.post(`/attendance/${id}`, attendanceData);
 export const deleteStudent = (id) => API.delete(`/students/${id}`);
-export const filterAttendance = (data) => API.get('/attendance', { params: data }); // Use query parameters for year and month
+export const updateStudent = (id, updatedStudent) =>API.put(`/students/${id}`, updatedStudent);
+export const filterAttendance = (params) => API.get('/attendance/filter', { params });
+export const fetchStudentNames = () => API.get('/attendance/names');
